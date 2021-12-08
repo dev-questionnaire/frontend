8f95b389cd04629432ad4300e0e05eb9ed2a2e9a
Philipp Hermes <philipphermes2001@gmail.com>
2021-12-08 08:18:32 +0100

    I expected something different.

---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Exam;
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+class AppFixtures extends Fixture
+{
+    private array $entityList = [];
+
+    public function __construct(private UserPasswordHasherInterface $userPasswordHasher)
+    {
+    }
+
+    public function load(ObjectManager $manager): void
+    {
+        $this->loadUser();
+        $this->loadExam();
+
+        foreach ($this->entityList as $entity) {
+            $manager->persist($entity);
+        }
+
+        $manager->flush();
+    }
+
+    private function loadUser(): void
+    {
+        $user = new User();
+
+        $user
+            ->setEmail('admin@email.com')
+            ->setRoles(['ROLE_ADMIN', 'ROLE_USER']);
+
+        $user->setPassword(
+            $this->userPasswordHasher->hashPassword(
+                $user, 'admin')
+        );
+
+        $this->entityList[] = $user;
+
+        $user = new User();
+
+        $user
+            ->setEmail('user@email.com')
+            ->setRoles(['ROLE_USER']);
+
+        $user->setPassword(
+            $this->userPasswordHasher->hashPassword(
+                $user, 'user')
+        );
+
+        $this->entityList[] = $user;
+    }
+
+    private function loadExam(): void
+    {
+        $exam = new Exam();
+
+        $exam
+            ->setName('SOLID');
+
+        $this->entityList[] = $exam;
+
+        $exam = new Exam();
+
+        $exam
+            ->setName('Testing');
+
+        $this->entityList[] = $exam;
+    }
+}
