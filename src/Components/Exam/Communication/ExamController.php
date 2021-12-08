868ee9b3be0196a639ea5713c2381c88602ce00f
Philipp Hermes <philipphermes2001@gmail.com>
2021-12-08 08:55:44 +0100

    We know the game and we're gonna play it

--- src/Components/Exam/Communication/ExamController.php
+++ src/Components/Exam/Communication/ExamController.php
@@ -2,20 +2,28 @@
 
 namespace App\Components\Exam\Communication;
 
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use App\Components\Exam\Persistence\Repository\ExamRepositoryInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ExamController extends AbstractController
 {
+    public function __construct(
+        private ExamRepositoryInterface $examRepository,
+    )
+    {
+    }
+
     /**
      * @Route ("/", name="app_exam")
      */
     public function index(): Response
     {
+        $examList = $this->examRepository->getAll();
+
         return $this->render('exam/exam.html.twig', [
-            'controller_name' => 'ExamController',
+            'examList' => $examList,
         ]);
     }
 }
