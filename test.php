a9abffe8b759436dc85feaf4c0f0e716c1383beb
Philipp Hermes <philipphermes2001@gmail.com>
2022-01-11 08:31:00 +0100

    Completed with no bugs...

--- test.php
+++
@@ -1,9 +0,0 @@
-<?php
-declare(strict_types=1);
-
-$dir = new DirectoryIterator(dirname(__FILE__));
-foreach ($dir as $fileinfo) {
-    if (!$fileinfo->isDot()) {
-        echo $fileinfo->getFilename() . PHP_EOL;
-    }
-}
